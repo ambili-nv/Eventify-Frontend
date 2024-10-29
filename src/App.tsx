c438102f79bf993ef1917e826013ded1d31413c1
ambili-nv <ambilinadvilvalappil@gmail.com>
2024-10-29 19:15:11 +0530

    few changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import { Toaster } from 'react-hot-toast';
 import { MainRouter } from './routes/router'
 import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Navbar';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
 
     <Router>
-      {/* <Header/> */}
     <Toaster/>
     <MainRouter/>
     </Router>
