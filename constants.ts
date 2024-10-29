f4795867311545f98bb687835c3bce36351d90db
ambili-nv <ambilinadvilvalappil@gmail.com>
2024-10-29 20:30:58 +0530

    few changes

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:5000/api"
-export const BASE_URL = "https://eventify-backend-hy8p.onrender.com/api"
+export const BASE_URL = "https://eventify-backend-sysc.onrender.com/api"
 export const nameRegex = /^[A-Z][a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
 export const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
 export const CLOUDINARY_UPLOAD_API = "https://api.cloudinary.com/v1_1/dwytsuspa/upload";
